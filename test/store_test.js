cabff4454371305229778edbfa94643cac9e70af
Jay Hagar <jaythagar@gmail.com>
2021-10-18 13:39:33 -0500

    Add Truffle and Drizzle files
    add files for truffle and drizzle from Truffle suite

---
+++ test/store_test.js
@@ -0,0 +1,13 @@
+const storeTest = artifacts.require("storeTest");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("storeTest", function (/* accounts */) {
+  it("should assert true", async function () {
+    await storeTest.deployed();
+    return assert.isTrue(true);
+  });
+});
